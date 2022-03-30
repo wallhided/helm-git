3a829ab97e35adbcb939a01ca7b79c99489c33d8
wallhided <261296dg@gmail.com>
2022-03-30 12:07:47 +0300

    Add ssh bitbucket

--- getfile.sh
+++ getfile.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 set -e
 
-URI=$@ # eg: gitlab://username/project:master/kubernetes/helm-chart
-PROVIDER=$(echo $URI | cut -d: -f1) # eg: gitlab
+URI=$@ # eg: gitlab://username/project:master/kubernetes/helm-chart, github-https://username/project:v1/path
+PROVIDER=$(echo $URI | cut -d: -f1) # eg: gitlab, bitbucket
 REPO=$(echo $URI | cut -d: -f2 | sed -e "s/\/\///") # eg: username/project
 BRANCH=$(echo $URI | cut -d: -f3 | cut -d/ -f1) # eg: master
 FILEPATH=$(echo $URI | cut -d: -f3 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
@@ -15,14 +15,20 @@ TMPDIR="$(mktemp -d)"
 cd $TMPDIR
 
 git init --quiet
-git remote add origin git@$PROVIDER.com:$REPO.git
+if [ "bitbucket" = $PROVIDER ]; then
+  git remote add origin git@bitbucket.org:imvu/$REPO.git
+else
+  git remote add origin git@$PROVIDER.com:$REPO.git
+fi
 git pull --depth=1 --quiet origin $BRANCH
 
 if [ -f $FILEPATH ]; then # if a file named $FILEPATH exists
   cat $FILEPATH
 else
-  echo "Error in plugin 'helm-git': $BRANCH:$FILEPATH does not exists" >&2
-  exit 1
+  echo "Root path" >&2
+  cat $FILEPATH
+  #echo "Error in plugin 'helm-git': $BRANCH:$FILEPATH does not exists" >&2
+  #exit 1
 fi
 
 # remove the temporary dir
